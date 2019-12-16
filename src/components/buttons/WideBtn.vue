99524bff009d9841d4124abb264e09fbee7f34e6
PiotrAcalski <piotr.acalski@gmail.com>
2019-12-16 01:22:03 +0100

    files missing in prev commit

---
+++ src/components/buttons/WideBtn.vue
@@ -0,0 +1,58 @@
+<template>
+  <button class="wideBtn" :id="parent+'-wideBtn-'+wideBtn.name" @click="emitWideBtnClick">
+      <span class="wideBtn-lbl">
+          {{
+              wideBtn.lbl
+          }}
+      </span>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'WideBtn',
+    props: ['wideBtn', 'parent'],
+    mounted() {
+        if (this.wideBtn.name === 'reset') {
+            document.getElementById(`${this.parent}-wideBtn-${this.wideBtn.name}`).classList.add('wideBtnDisabled')
+        }
+    },
+    methods:{
+        emitWideBtnClick: function () {
+            let wideBtnClassList = Array.from(document.getElementById(`${this.parent}-wideBtn-${this.wideBtn.name}`).classList);
+            if (!wideBtnClassList.includes('wideBtnDisabled')) {
+                this.$emit('wideBtnClick',this.wideBtn.name);
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    .wideBtn{
+        width: 100%;
+        height:40px;
+        background-color:#5d5974;
+        border:none;
+        box-shadow: 0 0 10px 0 #000;
+    }
+    .wideBtn:hover{
+        cursor: pointer;
+        border:none;
+        box-shadow: 0 0 10px 0 #41B883;
+    }
+    .wideBtnDisabled {
+        color: #a3a2aa;
+    }
+    .wideBtnDisabled:hover {
+        box-shadow: 0 0 10px 0 #000;
+        cursor: default;
+    }
+    .wideBtn-lbl {
+        font-family: 'Prompt', sans-serif;
+        font-size: 16px;
+        color: #f7f7f7;
+        line-height: 16px;
+        text-indent: 0px;
+        }
+</style>
---
+++ src/components/content/changePhoto/ChangePhoto.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="ChangePhoto-box">
+      <CpContent ref="cpContent" :user="user" :car="car"/>
+      <CpButtons v-on:btnClick="emitButtonClick"/>
+  </div>
+</template>
+
+<script>
+import CpButtons from './CpButtons.vue'
+import CpContent from './CpContent.vue'
+
+export default {
+    name: 'ChangePhoto',
+    components: {
+        CpButtons,
+        CpContent
+    },
+    props: ['user', 'car'],
+    methods: {
+        emitButtonClick: function(btn) {
+            this.$emit('btnClick',btn);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #ChangePhoto-box {
+        width: 400px;
+        height: 720px;
+        padding: 20px;
+        box-sizing: border-box;
+        background-color: #312D3C;
+        position:absolute;
+        z-index: 10;
+        margin: auto;
+        left: 50vw;
+        top: 50vh;
+        transform:translate(-200px, -360px);
+        overflow:hidden;
+        box-shadow: 0px 0px 50px 0px #000;
+    }
+</style>
